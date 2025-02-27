d42252e151dfb954fcbdd34d484bafc1e7ba8105
sivarajkmalai <sivarajkmalai@gmail.com>
2025-02-27 19:53:52 +0530

    updating file

---
+++ app/src/main/java/app/olauncher/MyAccessibilityService.kt
@@ -0,0 +1,12 @@
+package app.olauncher.helper
+
+import android.accessibilityservice.AccessibilityService
+import android.content.Intent
+import android.os.Build
+import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityNodeInfo
+import androidx.annotation.RequiresApi
+import app.olauncher.MainActivity
+import app.olauncher.R
+import app.olauncher.data.Prefs
+
