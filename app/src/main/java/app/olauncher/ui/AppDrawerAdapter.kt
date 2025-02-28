4d10cb2da5108e487729f002ecddc798bd98956e
sivarajkmalai <sivarajkmalai@gmail.com>
2025-02-28 11:15:14 +0530

    update -- block icon color

--- app/src/main/java/app/olauncher/ui/AppDrawerAdapter.kt
+++ app/src/main/java/app/olauncher/ui/AppDrawerAdapter.kt
@@ -13,6 +13,7 @@ import android.widget.Filter
 import android.widget.Filterable
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -189,6 +190,7 @@ class AppDrawerAdapter(
                 appBlock.setOnClickListener {
                     showBlockDurationDialog(root.context, appModel, appBlockListener)
                 }
+//                appBlock.setBackgroundColor(ContextCompat.getColor(root.context, R.color.))
 
                 appRename.setOnClickListener {
                     if (appModel.appPackage.isNotEmpty()) {
